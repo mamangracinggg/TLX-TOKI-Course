a7a564c765ecd0119eaf792341b4903c105c91b2
yoseph yusanto <120454307+mamangracinggg@users.noreply.github.com>
2023-09-20 13:47:24 +0700

    Create cara lain if then case.cpp
    chapter 5 problem e

---
+++ Pemrograman/05 . Percabangan/cara lain if then case.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    if(n < 10){
+        printf("satuan\n");
+    }else if(n >= 10){
+        if(n < 100){
+            printf("puluhan\n");
+        }else if(n >= 100){
+            if(n < 1000){
+                printf("ratusan\n");
+            }else if(n >= 1000){
+                if(n < 10000){
+                    printf("ribuan\n");
+                }else if(n >= 10000){
+                    printf("puluhribuan\n");
+                }
+            }
+        }
+    }
+    return 0;
+}
