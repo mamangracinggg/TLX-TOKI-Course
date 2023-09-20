e346242b277f69eb375ef50e521a6eab46ff1699
yoseph yusanto <120454307+mamangracinggg@users.noreply.github.com>
2023-09-20 15:01:22 +0700

    Update floor ceiling.cpp
    penambahasan kasus untuk n bil bulat, maka floor dan ceil sama sama bil bulat dengan nilai yang sama

--- Pemrograman/05 . Percabangan/floor ceiling.cpp
+++ Pemrograman/05 . Percabangan/floor ceiling.cpp
@@ -4,9 +4,11 @@ int main(){
     double n;
     scanf("%lf", &n);
     int ans = trunc(n); 
-    if(n >= 0){
+    if(n == ans){ //jika masukan n adalah bilangan bulat 
+        printf("%d %d\n", ans, ans);
+    }else if(n > 0){
         printf("%d %d\n", ans, ans + 1);
-    }else{
+    }else if(n < 0){
         printf("%d %d\n", ans - 1, ans);
     }
     return 0;
