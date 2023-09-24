3fe8555ae1cecd6026795ca424466dce1473abff
yoseph yusanto <120454307+mamangracinggg@users.noreply.github.com>
2023-09-24 12:12:59 +0700

    Create teman dekat.cpp
    chapter 10 problem f

---
+++ Pemrograman/10 . Subprogram/teman dekat.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+int pangkat(int a, int b){
+	int ans = 1;
+	for(int i = 0; i < b; i++){
+		ans = ans * a;
+	}
+	return ans;
+}
+
+int main(){
+	int n, d;
+	scanf("%d %d", &n, &d);
+	int x[n], y[n];
+	for(int i = 0; i < n; i++){
+		scanf("%d %d", &x[i], &y[i]);
+	}
+	int t[n][n];
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			t[i][j] = pangkat(abs(x[j] - x[i]), d) + pangkat(abs(y[j] - y[i]), d);
+		}
+	}
+	int terkecil = t[0][1];
+	int terbesar = t[0][1];
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			terkecil = min(terkecil, t[i][j]);
+			terbesar = max(terbesar, t[i][j]);
+		}
+	}
+	printf("%d %d\n", terkecil, terbesar);
+	return 0;
+}
